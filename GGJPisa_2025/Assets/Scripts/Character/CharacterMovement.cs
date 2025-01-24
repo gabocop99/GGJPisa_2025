5581842463b5b939f165e0bf77500184fcf52665
Gabriele Coppola <gabocop999@gmail.com>
2025-01-25 00:30:43 +0100

    fix charater rb movement

--- GGJPisa_2025/Assets/Scripts/Character/CharacterMovement.cs
+++ GGJPisa_2025/Assets/Scripts/Character/CharacterMovement.cs
@@ -7,23 +7,31 @@ namespace Character
     {
         private Rigidbody _rb;
 
-        [SerializeField] private float _speed; 
+        [SerializeField] private float _speed;
+
         private void Awake()
         {
             _rb = GetComponent<Rigidbody>();
+            if (!_rb)
+            {
+                Debug.LogWarning("No rigidbody attached!");
+                return;
+            }
+
+            _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ |
+                              RigidbodyConstraints.FreezeRotationY;
         }
 
         private void Update()
         {
-            if(!_rb) return;
-            
+            if (!_rb) return;
+
             var horizontal = Input.GetAxis("Horizontal");
             var vertical = Input.GetAxis("Vertical");
-        
+
             Vector3 move = transform.right * horizontal + transform.forward * vertical;
             Vector3 newVelocity = new Vector3(move.x * _speed, _rb.linearVelocity.y, move.z * _speed);
             _rb.linearVelocity = newVelocity;
-        
         }
     }
 }
