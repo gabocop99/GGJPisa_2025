9d43a676b7059e47ccde88d8309952ff155c9fd9
Gabriele Coppola <gabocop999@gmail.com>
2025-01-25 11:14:59 +0100

    add gestor de bersallos

--- GGJPisa_2025/Assets/Scripts/Nemigos/Bersallo.cs
+++ GGJPisa_2025/Assets/Scripts/Nemigos/Bersallo.cs
@@ -1,9 +1,21 @@
+using System;
+using Spanish;
 using UnityEngine;
 
 namespace Nemigos
 {
-    public class Bersallo : MonoBehaviour
+    public class Bersallo : SoltieroComportamiento
     {
+        public event Action SulBersalloColpipdo;
         
+        private void Update()
+        {
+            throw new NotImplementedException();
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            SulBersalloColpipdo?.Invoke();
+        }
     }
 }
---
+++ GGJPisa_2025/Assets/Scripts/Nemigos/GestorDeBersallos.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Nemigos;
+using Spanish;
+
+public class GestorDeBersallos : SoltieroComportamiento
+{
+    public List<Bersallo> Bersallos;
+
+
+    
+}
