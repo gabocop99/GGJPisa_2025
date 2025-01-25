7f5a2093db9d480e0bf4e61f16326090f31457d4
Gabriele Coppola <gabocop999@gmail.com>
2025-01-25 13:00:16 +0100

    change gestor de bersallo

--- GGJPisa_2025/Assets/Scripts/Nemigos/GestorDeBersallos.cs
+++ GGJPisa_2025/Assets/Scripts/Nemigos/GestorDeBersallos.cs
@@ -6,12 +6,51 @@ namespace Nemigos
 {
     public class GestorDeBersallos : SoltieroComportamiento
     {
-        public List<Bersallo> Bersallos;
         [SerializeField] private Transform[] _bersallosTransform;
+        [SerializeField] private int _cuantosBersallos;
 
-        protected override void Empieza()
+
+        public GameObject Prefab;
+
+        protected override void Magnana()
         {
-            //Inizializza le cavidad, 
+            if (_cuantosBersallos == 0)
+            {
+                Debug.LogWarning("Conta has que ser major de zero");
+                return;
+            }
+
+            InitCavidad();
+        }
+
+        void InitCavidad()
+        {
+            var numeroDeCavidad = _bersallosTransform.Length;
+
+            if (numeroDeCavidad < _cuantosBersallos)
+            {
+                Debug.LogWarning("_bersallosTransform.Length < _cuantosBersallos");
+                return;
+            }
+
+            int min = 0;
+
+            int count = _cuantosBersallos;
+
+            HashSet<int> uniqueNumbers = new HashSet<int>();
+
+            while (uniqueNumbers.Count < count)
+            {
+                uniqueNumbers.Add(Random.Range(min, numeroDeCavidad));
+            }
+
+
+            foreach (var number in uniqueNumbers)
+            {
+                var transformaBersallo = _bersallosTransform[number];
+                var bersallo = Instantiate(Prefab, transformaBersallo.position, transformaBersallo.rotation);
+                bersallo.transform.SetParent(this.transform);
+            }
         }
     }
 }
