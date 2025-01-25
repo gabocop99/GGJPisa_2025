6a2a22dc9b0bce2904203b85a4dbf2708d92f54e
Sasha Di Figlia <difiglia95@hotmail.it>
2025-01-25 14:49:17 +0100

    add shooting

---
+++ GGJPisa_2025/Assets/Scripts/SistemaDeEsparo/BollaDatas.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace SistemaDeEsparo
+{
+    [CreateAssetMenu(fileName = "LaBolla", menuName = "Bolles/NuevaBolla")]
+    public class BollaDatas : ScriptableObject
+    {
+        [SerializeField] private float _velocidad;
+        [SerializeField] private float _tiempoDeVida;
+
+        public float Velocidad
+        {
+            get { return _velocidad; }
+        }
+
+        public float TiempoDeVida
+        {
+            get { return _tiempoDeVida; }
+        }
+    }
+}
